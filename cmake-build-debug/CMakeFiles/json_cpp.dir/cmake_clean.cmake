a5df0632fb9bb5ac20fc6cd65a948d387435a8f3
laiiihz <laiiihz@outlook.com>
2018-11-07 08:17:32 +0000

    init

---
+++ cmake-build-debug/CMakeFiles/json_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/json_cpp.dir/main.cpp.o"
+  "json_cpp.pdb"
+  "json_cpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/json_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
