85368b5aea0749463bb60d511442edef4e0b532e
laiiihz <laiiihz@outlook.com>
2018-11-17 15:48:06 +0800

    fix

--- lightjson.cpp
+++ lightjson.cpp
@@ -11,10 +11,44 @@ class light_context{
 public:const char* json;
 };
 
-int light_parse(light_value *value, const char* json){
+static void light_parse_whitespace(light_context* c){
+    const char* p=c->json;
+    while(*p==' '||*p=='\t'||*p=='\r')p++;
+    c->json=p;
+}
 
+static int light_parse_null(light_context* c,light_value* value){
+    EXPECT(c,'n');
+    if(c->json[0]!='u'||c->json[1]!='l'||c->json[2]!='l')return LIGHT_PARSE_INVALID_VALUE;
+    c->json+=3;
+    value->type=LIGHT_NULL;
+    return LIGHT_PARSE_OK;
+}
+static int light_parse_true(light_context* c,light_value* value){
+    EXPECT(c,'t');
+    //todo fix light parse true
+}
+
+//todo add light parse false
+
+static int light_parse_value(light_context* c,light_value* value){
+    switch (*c->json){
+        case 'n':return light_parse_null(c,value);
+        case '\0':return LIGHT_PARSE_EXPECT_VALUE;
+        default:return LIGHT_PARSE_INVALID_VALUE;
+    }
+}
+
+int light_parse(light_value *value, const char* json){
+    light_context c;
+    assert(value!=NULL);
+    c.json=json;
+    value->type=LIGHT_NULL;
+    light_parse_whitespace(&c);
+    return light_parse_value(&c,value);
 }
 
 light_type light_get_type(const light_value *value){
-
+    assert(value!=NULL);
+    return value->type;
 }
--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@
 #include <iomanip>
 #include "lightjson.h"
 
-static int main_ret=0;
+static int main_ret=0;      //主函数返回值
 static int test_count=0;    //测试项目数
 static int test_pass=0;     //测试通过数
 
@@ -31,13 +31,34 @@ static int test_pass=0;     //测试通过数
         EXPECT_EQ_INT(LIGHT_PARSE_OK,light_parse(&value,"null"));
         EXPECT_EQ_INT(LIGHT_NULL,light_get_type(&value));
     }
-
+    static void test_parse_expect_value(){
+        light_value value;
+        value.type=LIGHT_FALSE;
+        EXPECT_EQ_INT(LIGHT_PARSE_EXPECT_VALUE,light_parse(&value,""));
+        EXPECT_EQ_INT(LIGHT_NULL,light_get_type(&value));
+    }
+    static void test_parse_invalid_value(){
+        light_value value;
+        value.type=LIGHT_FALSE;
+        EXPECT_EQ_INT(LIGHT_PARSE_INVALID_VALUE,light_parse(&value,"nul"));
+        EXPECT_EQ_INT(LIGHT_NULL,light_get_type(&value));
+    }
+    static void test_parse_root_not_singular(){
+        light_value value;
+        value.type=LIGHT_FALSE;
+        EXPECT_EQ_INT(LIGHT_PARSE_ROOT_NOT_SINGULAR,light_parse(&value,"null x"));
+        EXPECT_EQ_INT(LIGHT_NULL,light_get_type(&value));
+    }
     static void test_parse(){
         test_parse_null();
+        test_parse_expect_value();
+        test_parse_invalid_value();
+        test_parse_root_not_singular();
     }
 
     int main(){
         test_parse();
         std::cout<<test_pass<<"/"<<test_count<<"("
-        <<std::fixed<<std::setprecision(2)<<test_pass/test_count*100<<"% passed)";
+        <<std::fixed<<std::setprecision(10)<<test_pass*100.0/test_count<<"% passed)";
+        return main_ret;
     }
